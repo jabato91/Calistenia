be80cfb0360df476e7c56a2a61da3500cdcc3e62
javie <javie@DESKTOP-F9R330A>
2025-09-21 15:22:34 +0200

    Agregar archivos de proyecto.

---
+++ ProyectoFinDeCurso/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ProyectoFinDeCurso
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ProyectoFinDeCurso/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ProyectoFinDeCurso
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
