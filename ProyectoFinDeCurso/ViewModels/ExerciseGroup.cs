aee02f13270aea7dd00035a3801b7c865e0bc90a
Practicas <Practicas@DESKTOP-OUDFIBB>
2025-11-26 11:26:19 +0100

    Eliminar

--- ProyectoFinDeCurso/ViewModels/ExerciseGroup.cs
+++ ProyectoFinDeCurso/ViewModels/ExerciseGroup.cs
@@ -1,22 +1,12 @@
-﻿using ProyectoFinDeCurso.Enums;
-using ProyectoFinDeCurso.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
- namespace ProyectoFinDeCurso.ViewModels
+namespace ProyectoFinDeCurso.Pages.Main
 {
-    public class ExerciseGroup : List<Exercise>
+    internal class ExerciseGroup
     {
-        public bodyPartEnum MuscleGroupId { get; set; } // Identificador del grupo muscular
-        public string DisplayName => MuscleGroupId.ToFriendlyName(); // Nombre amigable del grupo muscular
-
-        public ExerciseGroup(bodyPartEnum muscleGroupId, IEnumerable<Exercise> exercises)
-            : base(exercises) // ordena los ejercicios por grupo muscular
-        {
-            MuscleGroupId = muscleGroupId;
-        }
     }
 }
